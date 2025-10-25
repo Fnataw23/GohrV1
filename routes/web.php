3f8b0d81cb112290bafa33fdd7acd92bbfc91d00
Mikhail <misha.shuvaev15@gmail.com>
2025-10-25 17:53:23 +0300

    new migration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Auth::routes();
+Route::get('/', [App\Http\Controllers\MainController::class, 'index'])->name('/');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
